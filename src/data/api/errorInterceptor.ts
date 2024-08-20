77fe56badfcb2aac873d0dbaac9a75f8819e32b9
Ashish <ashish.kumar@1bridge.one>
2024-08-20 22:54:43 +0530

    removed unwanted code

--- src/data/api/errorInterceptor.ts
+++ src/data/api/errorInterceptor.ts
@@ -1,7 +1,6 @@
 const errorInterceptor = async (error: any): Promise<any> => {
   const statusCode = error.response.status;
   if (statusCode === 401) {
-    console.log('data');
     alert(error['message']);
   }
   return Promise.reject(error);
